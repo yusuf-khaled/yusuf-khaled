10f65980db339fb4d5ab6920525b45d98ef733f1
Yusuf <yhbk12@gmail.com>
2019-09-30 13:06:07 -0400

    Updates

---
+++ precache-manifest.768f164f2326705f8305f90598828b6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b9e4f53ceb3de9eb052a2280327596",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "9117a1e441fc7c17d0e8",
+    "url": "/site/static/css/main.40397388.chunk.css"
+  },
+  {
+    "revision": "8a13674d190b84fbd1e7",
+    "url": "/site/static/js/2.ea729f77.chunk.js"
+  },
+  {
+    "revision": "9117a1e441fc7c17d0e8",
+    "url": "/site/static/js/main.12ea11dc.chunk.js"
+  },
+  {
+    "revision": "9f57f323bd85d478512d",
+    "url": "/site/static/js/runtime-main.0a8bd017.js"
+  }
+]);
